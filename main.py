03e83aac17346868e13722b61b27d86e2a1f4dae
Alexandre <ale.a.braz@gmail.com>
2025-08-21 15:05:52 -0300

    terceiro

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
-import pygame
+from code.Game import Game
 
-print('Setup Star')
-pygame.init()
-window = pygame.display.set_mode(size=(600, 480))
-print('Setup End')
-
-print('Loop Star')
-while True:
-    # check for all events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            print('Quitting...')
-            pygame.quit() # close window
-            quit() # end pygame
+game = Game()
+game.run()
